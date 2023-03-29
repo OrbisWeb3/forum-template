a96a4585679df5dd2f1417237e9c52cea3b69883
- <53183727+baptistegreve@users.noreply.github.com>
2023-03-29 10:58:26 +0200

    Added the community chat context as a global variable

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,11 @@ import '@/styles/globals.css'
 import { Orbis, OrbisProvider } from "@orbisclub/components";
 import "@orbisclub/components/dist/index.modern.css";
 
-/** Set the global forum context here */
+/** Set the global forum context here (you can create categories using the dashboard by clicking on "Create a sub-context" from your main forum context) */
 global.orbis_context = "kjzl6cwe1jw147eabkq3k4z6ka604w0xksr5k9ildy1glfe1ebkcfmtu8k2d94j";
+
+/** Set the global chat context here (the chat displayed when users click on the "Community Chat" button) */
+global.orbis_chat_context = "kjzl6cwe1jw147040w6bj3nkvny3ax30q76ib5ytxo6298psrx1oawa3wmme2jx";
 
 let orbis = new Orbis({
   useLit: false,
